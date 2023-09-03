c21266621d4e05cbd4ca3db46117f9fd634a265d
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-09-03 20:26:25 +0200

    Create generateCell function

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
+<?php declare (strict_types = 1)?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Maze Generator</title>
 </head>
 <body>
-   <?php
-echo "test";
+
+
+        <?php
+function generateCell(int $numbHorizontalCell, int $numbVerticalCell)
+{
+    echo "This is a $numbHorizontalCell by $numbVerticalCell maze";
+
+    $totalCell = $numbHorizontalCell * $numbVerticalCell;
+
+    echo "<div class='grid-container'>";
+    for ($i = 0; $i < $totalCell; $i++) {
+        echo "<div class='maze-cell'>$i</div>";
+    }
+    echo "</div>";
+
+}
+generateCell(6, 6);
 ?>
 </body>
 </html>
