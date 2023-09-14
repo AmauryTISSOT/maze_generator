ca249dece1097cefd243baa6b1e7ca44c15fa919
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-09-14 21:29:56 +0200

    Add Cell & DimensionalArray class

--- index.php
+++ index.php
@@ -38,6 +38,104 @@ function generateCell(int $numbHorizontalCell, int $numbVerticalCell)
 }
 
 generateCell(6, 6);
+
+class Cell
+{
+    public $x;
+    public $y;
+    public $wall = [true, true, true, true];
+    public $visited = false;
+
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function getCordinate()
+    {
+        echo "($this->x, $this->y) ";
+
+    }
+
+}
+
+class DimensionalArray
+{
+    public $array = [];
+    public $rows = 3;
+    public $columns = 3;
+    public $numberOfCell;
+
+    public function __construct()
+    {
+        $this->numberOfCell = $this->rows * $this->columns;
+    }
+
+    public function setRows($i)
+    {
+        $this->rows = $i;
+    }
+
+    public function setColumns($i)
+    {
+        $this->columns = $i;
+    }
+
+    public function getArray()
+    {
+        return $this->array;
+    }
+
+    public function consoleLogArray()
+    {
+        echo "<script>console.log('" . json_encode($this->array) . "')</script>";
+    }
+
+    public function create2DArray($object)
+    {
+        for ($i = 0; $i < $this->rows; $i++) {
+            $this->array[$i] = [];
+            for ($j = 0; $j < $this->columns; $j++) {
+                $this->array[$i][$j] = $object;
+            }
+        }
+    }
+}
+
+$grid = new DimensionalArray();
+for ($i = 0; $i < 3; $i++) {
+    echo "</br>";
+    for ($j = 0; $j < 3; $j++) {
+        $cell = new Cell($i, $j);
+        $grid->array[$i][$j] = $cell;
+        $cell->getCordinate();
+
+    }
+}
+
+$grid->consoleLogArray();
+
+$grid->array[0][0]->visited = true;
+
+function checkIfVisited($arr, $x, $y)
+{
+
+    if ($arr->array[$y][$x]->visited) {
+        echo "</br>($x,$y) has been visited";
+        return true;
+    } else {
+        echo "</br>($x,$y) not visited";
+        return false;
+    }
+}
+
+//echo $grid->array[0][0]->visited;
+
+checkIfVisited($grid, 1, 0);
+
+$grid->consoleLogArray();
+
 ?>
 </body>
 </html>
