ea5f1649289442ef6856cc8c76f85fc9fe9ff6b6
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-09-16 22:17:46 +0200

    Create DisplayCell class

---
+++ DisplayCell.php
@@ -0,0 +1,32 @@
+<?php
+class DisplayCell
+{
+    public $array = [];
+    private $wallArray = ["Top", "Bottom", "Left", "Right"];
+
+    public function generateAssociativeArray(int $numberOfKeys)
+    {
+        for ($i = 0; $i <= $numberOfKeys; $i++) {
+            $this->array[$i] = $this->wallArray;
+        }
+        return $this->array;
+    }
+
+    public function generateCell(int $numbHorizontalCell, int $numbVerticalCell)
+    {
+        echo "This is a $numbHorizontalCell by $numbVerticalCell maze";
+
+        $totalCell = $numbHorizontalCell * $numbVerticalCell;
+        $gridWidth = $numbHorizontalCell * 50;
+        $gridHeight = $numbVerticalCell * 50;
+
+        echo "<div class='grid-container' style='grid-template-columns: repeat($numbHorizontalCell, 1fr); grid-template-rows: repeat($numbVerticalCell, 1fr); width: {$gridWidth}px; height: {$gridHeight}px;'>";
+        for ($i = 0; $i < $totalCell; $i++) {
+            echo "<div class='maze-cell'>$i</div>";
+        }
+        echo "</div>";
+
+        $this->generateAssociativeArray($totalCell);
+    }
+
+}
