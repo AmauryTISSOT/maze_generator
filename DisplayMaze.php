9de541bfa113168f38bf1513a2f62e994202933a
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-09-24 18:50:08 +0200

    Rework class

--- DisplayMaze.php
+++ DisplayMaze.php
@@ -2,36 +2,42 @@
 class DisplayMaze
 {
     public $array = [];
-    private $wallArray = ["Top", "Bottom", "Left", "Right"];
+    public $numberOfHorizontalCell;
+    public $numberOfVerticalCell;
+    //private $wallArray = ["Top", "Bottom", "Left", "Right"];
     public $showCellNumber = false;
 
-    public function generateAssociativeArray(int $numberOfKeys)
+    public function __construct($arrayParam)
     {
-        for ($i = 0; $i <= $numberOfKeys; $i++) {
-            $this->array[$i] = $this->wallArray;
-        }
-        return $this->array;
+        $this->array = $arrayParam;
+        $this->numberOfHorizontalCell = count($this->array);
+        $this->numberOfVerticalCell = count($this->array[0]);
     }
 
-    public function generateCell(int $numbHorizontalCell, int $numbVerticalCell)
+    public function showMaze()
     {
-        echo "This is a $numbHorizontalCell by $numbVerticalCell maze";
+        $gridWidth = $this->numberOfHorizontalCell * 50;
+        $gridHeight = $this->numberOfVerticalCell * 50;
 
-        $totalCell = $numbHorizontalCell * $numbVerticalCell;
-        $gridWidth = $numbHorizontalCell * 50;
-        $gridHeight = $numbVerticalCell * 50;
+        // grid-container start
+        echo "<div class='grid-container' style='grid-template-columns: repeat($this->numberOfHorizontalCell, 1fr); grid-template-rows: repeat($this->numberOfVerticalCell, 1fr); width: {$gridWidth}px; height: {$gridHeight}px; back'>";
 
-        echo "<div class='grid-container' style='grid-template-columns: repeat($numbHorizontalCell, 1fr); grid-template-rows: repeat($numbVerticalCell, 1fr); width: {$gridWidth}px; height: {$gridHeight}px;'>";
-        for ($i = 0; $i < $totalCell; $i++) {
-            if ($this->showCellNumber) {
-                echo "<div class='maze-cell'>$i</div>";
-            } else {
-                echo "<div class='maze-cell'></div>";
+        for ($i = 0; $i < count($this->array); $i++) {
+            for ($j = 0; $j < count($this->array); $j++) {
+
+                $backgroundColor = $this->array[$i][$j]->visited ? "red" : "";
+
+                echo "<div class='maze-cell' id ='$i-$j'style='grid-row:" . ($this->array[$i][$j]->x) + 1 . " / " . $i + 2 . "; grid-column:" . $this->array[$i][$j]->y + 1 . " / " . $j + 2 . "; background-color: $backgroundColor;
+                ' >";
+                if ($this->showCellNumber) {
+                    echo "$i,$j";
+                }
+                echo "</div>";
             }
         }
-        echo "</div>";
 
-        $this->generateAssociativeArray($totalCell);
+        // grid-container end
+        echo "</div>";
     }
 
 }
