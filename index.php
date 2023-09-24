472c4eb5488e2a7712939c7739fb43468c94ef44
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-09-24 18:49:40 +0200

    Remove useless code and add TODO

--- index.php
+++ index.php
@@ -14,43 +14,20 @@ require 'Cell.php';
 require 'DimensionalArray.php';
 require 'DisplayMaze.php';
 
-$maze = new DisplayMaze();
-$maze->showCellNumber = true;
-$maze->generateCell(6, 6);
-
+// initialize a new 2D dimensional Array
 $grid = new DimensionalArray();
 
+//TODO: move this part to DimensionalArray class
 for ($i = 0; $i < 3; $i++) {
-    echo "</br>";
     for ($j = 0; $j < 3; $j++) {
         $cell = new Cell($i, $j);
         $grid->array[$i][$j] = $cell;
-        $cell->getCoordinate();
     }
 }
 
-function getNeighbourCells($x, $y, $objet)
-{
-    $top = $objet->array[$x - 1][$y];
-    $right = $objet->array[$x][$y + 1];
-    $bottom = $objet->array[$x + 1][$y];
-    $left = $objet->array[$x][$y - 1];
-
-    $top->visited = true;
-    $right->visited = true;
-    $bottom->visited = true;
-    $left->visited = true;
-}
-
-$grid->consoleLogArray();
-
-$grid->setVisited(0, 0);
-
-$grid->checkIfVisited(0, 0);
-
-getNeighbourCells(1, 1, $grid);
-
-$grid->consoleLogArray();
+$maze = new DisplayMaze($grid->array);
+$maze->showCellNumber = true;
+$maze->showMaze();
 
 ?>
 </body>
