676148c21354243afc7c0083087c6734c004cb48
AmauryTISSOT <104317431+AmauryTISSOT@users.noreply.github.com>
2023-10-07 19:53:18 +0200

    Add border handling method

--- DisplayMaze.php
+++ DisplayMaze.php
@@ -14,6 +14,31 @@ class DisplayMaze
         $this->numberOfVerticalCell = count($this->array[0]);
     }
 
+    private function borderHandling($i, $j)
+    {
+        $arr = [];
+
+        $cssBorderTop = 'border-top: 2px solid black;';
+        $cssBorderRight = 'border-right: 2px solid black;';
+        $cssBorderBottom = 'border-bottom: 2px solid black;';
+        $cssBorderLeft = 'border-left: 2px solid black;';
+
+        if ($this->array[$i][$j]->wall[0]) {
+            $arr[] = $cssBorderTop;
+        }
+        if ($this->array[$i][$j]->wall[1]) {
+            $arr[] = $cssBorderRight;
+        }
+        if ($this->array[$i][$j]->wall[2]) {
+            $arr[] = $cssBorderBottom;
+        }
+        if ($this->array[$i][$j]->wall[3]) {
+            $arr[] = $cssBorderLeft;
+        }
+
+        return implode(" ", $arr);
+    }
+
     public function showMaze()
     {
         $gridWidth = $this->numberOfHorizontalCell * 50;
@@ -27,8 +52,15 @@ class DisplayMaze
 
                 $backgroundColor = $this->array[$i][$j]->visited ? "red" : "";
 
-                echo "<div class='maze-cell' id ='$i-$j'style='grid-row:" . ($this->array[$i][$j]->x) + 1 . " / " . $i + 2 . "; grid-column:" . $this->array[$i][$j]->y + 1 . " / " . $j + 2 . "; background-color: $backgroundColor; font-size: 14px;
-                ' >";
+                echo "<div class='maze-cell' id ='$i-$j'
+                style=
+                '
+                grid-row:" . ($this->array[$i][$j]->x) + 1 . " / " . $i + 2 . ";
+                grid-column:" . $this->array[$i][$j]->y + 1 . " / " . $j + 2 . ";
+                background-color: $backgroundColor;
+                font-size: 14px;" . $this->borderHandling($i, $j) . "
+                '
+                 >";
                 if ($this->showCellNumber) {
                     echo "X$i,Y$j";
                 }
